c3e0efbfe959ef2030996d1dddab385a2d097e9a
MegSemrad <mlsemrad753@gmail.com>
2020-12-02 13:26:59 -0600

    finished dog breed section

--- practice.js
+++ practice.js
@@ -1,9 +1,9 @@
+/* Introduction : Age In Dog Years */
+
 /* 
-Paramter is a variable that will hold the value of the input
-
-A pure function means a function that does its job and RETURNS a value
-
-MUST HAVE: return & a variable to catch the result of the invoked function
+- Paramter is a variable that will hold the value of the input
+- A pure function means a function that does its job and RETURNS a value
+- MUST HAVE: return & a variable to catch the result of the invoked function
 */
 
 const  calculateAgeInDogYears = (ageOfPerson) => {
@@ -13,3 +13,24 @@ const  calculateAgeInDogYears = (ageOfPerson) => {
 
 const dogAge = calculateAgeInDogYears(22);
 console.log(dogAge);
+
+
+
+
+/* Practice : Best In Show */
+
+/*
+- Do not forget the === triple equal sign when using if statements
+*/
+
+const favoritePet = (petString) => {
+    if(petString === "meow") {
+        return "I like cats"
+    }
+    else {
+        return `My favorite dog breed is ${petString}.`
+    }
+;}
+
+const myFavorite = favoritePet("rottweiler");
+console.log("When it comes to pets, " + myFavorite);
